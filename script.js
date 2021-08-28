3a11f5113a43d51ebce81212fbcf5e29b388349c
Kevin Tran <kevin_tran95@yahoo.com>
2021-08-28 15:15:22 -0700

    added 5 day forcast and save search function

--- script.js
+++ script.js
@@ -20,3 +20,76 @@ var getCityWeather = function(city){
         });
     });
 };
+
+var submitHandler = function(event) {
+    event.preventDefault()
+        var city = cityInputEl.value.trim();
+        if (city){
+            getCityWeather(city);
+            getfiveDay(city);
+            cities.unshift({city});
+            cityInputEl.value = "";
+        } else{
+            alert("You must enter a city")
+        }
+        saveSearch();
+        pastSearch();
+}
+ var saveSearch = function(){
+     localStorage.setItem("cities", JSON.stringify(cities))
+ }
+
+ var getfiveDay = function(city) {
+     var apiKey = "694f5009d1a6972628a7c5264c2d4fd9"
+     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
+     fetch(apiUrl)
+     .then(function(response) {
+         response.JSON().then(function(data) {
+             displayFiveDay(data);
+         });
+     });
+ };
+
+ var displayFiveDay = function(weather) {
+     forcastContainerEl.textContent = ""
+     forcastTitle.textContent = "5-day Forcast:";
+     
+     var forcast = weather.list;
+     for (var i =5; i < forcast.length; i = i + 8){
+         var dailyForcast = forcast [i];
+
+         var forcastEl =  document.createElement('div');
+         forcastEl.classList = 'card bg-primary text-light m-2';
+         
+         var forcastDate = document.createElement('h4');
+         forcastDate.textContent= moment.unix(dailyForcast.dt).format("MMM D YYYY")
+         forcastDate.classList = 'card-header text-center';
+         forcastEl.appendChild(forcastDate);
+
+         var weatherIcon = document.createElement('img');
+         weatherIcon.classList = 'card-body text-center';
+         weatherIcon.setAttribute("src", "https://openweathermap.org/img/w/10d.png")
+         forcastEl.appendChild(weatherIcon)
+
+         var forcastTempEl = document.createElement('span');
+         forcastTempEl.classList = 'card-body text-center';
+         forcastTempEl.textContent = dailyForcast.main.temp + " F";
+         forcastEl.appendChild(forcastTempEl);
+
+         var windSpeedEl = document.createElement('span');
+         windSpeedEl.class = 'card-body text-center';
+         windSpeedEl.textContent = dailyForcast.main.wind + ' MPH';
+         forcastEl.appendChild(windSpeedEl);
+
+         var humidityEl = document.createElement('span');
+         humidityEl.class = 'card-body text-center';
+         humidityEl.textContent = dailyForcast.main.humidity + ' %';
+         forcastEl.appendChild(humidityEl);
+
+         forcastContainerEl.appendChild(forcastEl);
+
+
+
+     }
+ }
